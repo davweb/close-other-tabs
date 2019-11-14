f49c00cadc363436a50c51b75301cc89ad04fb50
David Webb <dave@davidwebb.me.uk>
2019-11-14 17:32:21 +0000

    Translations
    1. Move extension code into translation directory
    2. Add translations in CSV file
    3. Add python script to generate JSON files from CSV
    4. Ignore generated JSON files

---
+++ translations/translations.py
@@ -0,0 +1,36 @@
+import csv
+import os
+
+TEMPLATE = """{{
+    "extensionName": {{
+        "message": "{extensionName}",
+        "description": "Extension name"
+    }},
+    "extensionDescription": {{
+        "message": "{extensionDescription}",
+        "description": "Extension description"
+    }},
+    "contextMenuItemLabel": {{
+        "message": "{contextMenuItemLabel}",
+        "description": "Context menu item label for closing other tabs"
+    }}
+}}
+"""
+
+with open('translations.csv') as csvfile:
+    # Skip header and en
+    csvfile.readline()
+    csvfile.readline()
+
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        (code, language, extensionName, extensionDescription, contextMenuItemLabel) = row
+        directory = "../_locales/{}".format(code)
+        
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        file = directory + "/messages.json"
+        with open(file, "w") as json_file:
+           json_file.write(TEMPLATE.format(**locals()))
+
