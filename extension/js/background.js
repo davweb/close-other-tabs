47c8394199c529d7a103a0d0e4351562d67f9a8f
David Webb <dave@davidwebb.me.uk>
2019-11-14 17:44:12 +0000

    Version 1.0
    1. Set version in manifest
    2. Remove debug

--- extension/js/background.js
+++ extension/js/background.js
@@ -11,9 +11,6 @@ const menuItemId = chrome.contextMenus.create({
 });
 
 function countTabs(windowId) {
-    //TODO Remove logging
-    console.log("count", windowId);
-
     chrome.tabs.getAllInWindow(windowId, function(tabs) {
         const enabled = tabs.length > 1;
         chrome.contextMenus.update(menuItemId, {
@@ -23,23 +20,16 @@ function countTabs(windowId) {
 }
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-    //TODO Remove logging
-    console.log("activated", activeInfo);
     countTabs(activeInfo.windowId);
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    //TODO Remove logging
-    console.log("removed", tabId, removeInfo);
     if (!removeInfo.isWindowClosing) {
         countTabs(removeInfo.windowId);
     }
 });
 
 chrome.windows.onFocusChanged.addListener(function(windowId) {
-    //TODO Remove logging
-    console.log("focus", windowId);
-
     if (windowId !== -1) {
         countTabs(windowId);
     }
